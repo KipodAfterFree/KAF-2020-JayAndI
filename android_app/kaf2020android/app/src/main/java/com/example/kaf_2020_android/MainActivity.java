ecae89e83e8c7b2a74d474a788f9f03634ae5f5b
1byte <sudaiguy1@gmail.com>
2020-10-10 17:01:22 +0300

    Finished server integration

--- android_app/kaf2020android/app/src/main/java/com/example/kaf_2020_android/MainActivity.java
+++ android_app/kaf2020android/app/src/main/java/com/example/kaf_2020_android/MainActivity.java
@@ -1,69 +1,97 @@
 package com.example.kaf_2020_android;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import androidx.appcompat.app.AppCompatActivity;
 import java.io.IOException;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    /* access modifiers changed from: protected */
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.authenticate);
-
-        Button login_button = (Button)findViewById(R.id.login_button);
-
-        final EditText email_text = (EditText)findViewById(R.id.input_email);
-        final EditText password_text = (EditText)findViewById(R.id.input_password);
+        final EditText email_text = (EditText) findViewById(R.id.input_email);
+        final EditText password_text = (EditText) findViewById(R.id.input_password);
+        final ServerIntegrate server = new ServerIntegrate();
+        final Button login_button = (Button) findViewById(R.id.login_button);
+        final Button register_button = (Button) findViewById(R.id.register_button);
 
         login_button.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
-                String password = password_text.getText().toString();
-                String email = email_text.getText().toString();
+                final String password = password_text.getText().toString();
+                final String email = email_text.getText().toString();
                 if (password.length() == 0 || email.length() == 0) {
-                    Toast.makeText(MainActivity.this, "Password and email can't be empty!", Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                String login_status;
-
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            sendRequest("https://postb.in/1602108915311-2249859930016");
-                        } catch (IOException e) {
-                            e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "Password and email can't be empty!", 1).show();
+                } else {
+                    new Thread(new Runnable() {
+                        public void run() {
+                            try {
+                                if (server.login(email, password)) {
+                                    MainActivity.this.showToast("Login has succeeded!");
+                                } else {
+                                    MainActivity.this.showToast("Login has failed!");
+                                }
+                            } catch (IOException e) {
+                                MainActivity.this.showToast("Failed to connect to the server, make sure your internet is working!");
+                                e.printStackTrace();
+                            }
                         }
+                    }).start();
+                }
+            }
+        });
+        register_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                final EditText age_input = new EditText(MainActivity.this);
+                age_input.setHint("Age");
+                AlertDialog show = new AlertDialog.Builder(MainActivity.this).setTitle("Confirm age").setView(age_input).setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        final String age = age_input.getText().toString();
+                        if (!MainActivity.is_numeric(age)) {
+                            MainActivity.this.showToast("Age must be numeric");
+                            return;
+                        }
+                        final String password = password_text.getText().toString();
+                        final String email = email_text.getText().toString();
+                        new Thread(new Runnable() {
+                            public void run() {
+                                try {
+                                    if (server.register(email, password, age)) {
+                                        MainActivity.this.showToast("Login has succeeded!");
+                                    } else {
+                                        MainActivity.this.showToast("Login has failed!");
+                                    }
+                                } catch (IOException e) {
+                                    MainActivity.this.showToast("Failed to connect to the server, make sure your internet is working!");
+                                    e.printStackTrace();
+                                }
+                            }
+                        }).start();
                     }
-                }).start();
-
-
-                Toast.makeText(MainActivity.this, "Failed to login", Toast.LENGTH_LONG).show();
+                }).setNegativeButton("Cancel", (DialogInterface.OnClickListener) null).show();
             }
         });
     }
 
-    String sendRequest(String url) throws IOException {
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        try (Response response = client.newCall(request).execute()) {
-            return response.body().string();
+    public static boolean is_numeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
+    }
+
+    public void showToast(final String desc) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(MainActivity.this, desc, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ android_app/kaf2020android/app/src/main/java/com/example/kaf_2020_android/ServerIntegrate.java
@@ -0,0 +1,48 @@
+package com.example.kaf_2020_android;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class ServerIntegrate {
+    final String server_url = "http://10.0.0.1:12345";
+
+    /* access modifiers changed from: package-private */
+    public Response sendRequest(String base_url, String endpoint, Map<String, String> params) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        try {
+            HttpUrl.Builder urlBuilder = ((HttpUrl) Objects.requireNonNull(HttpUrl.parse(base_url + endpoint))).newBuilder();
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                urlBuilder.addQueryParameter(entry.getKey(), entry.getValue());
+            }
+            Response response = client.newCall(new Request.Builder().url(urlBuilder.build().toString()).build()).execute();
+            return response;
+        } catch (NullPointerException e) {
+            return null;
+        }
+    }
+
+    /* access modifiers changed from: package-private */
+    public Boolean register(String username, String password, String age) throws IOException {
+        Map<String, String> params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+        params.put("age", age);
+        Response resp = sendRequest(server_url, "/register", params);
+        return resp.code() == 200;
+    }
+
+    /* access modifiers changed from: package-private */
+    public Boolean login(String username, String password) throws IOException {
+        Map<String, String> params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+        Response resp = sendRequest(server_url, "/login", params);
+        return resp.code() == 200;
+    }
+}
