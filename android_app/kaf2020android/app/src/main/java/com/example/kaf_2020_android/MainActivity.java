699b4bed62238d0449c8a7b90b34b7217e2faecb
1byte <sudaiguy1@gmail.com>
2020-10-10 17:44:42 +0300

    Fixed bugs

--- android_app/kaf2020android/app/src/main/java/com/example/kaf_2020_android/MainActivity.java
+++ android_app/kaf2020android/app/src/main/java/com/example/kaf_2020_android/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kaf_2020_android;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -62,7 +63,8 @@ public class MainActivity extends AppCompatActivity {
                             public void run() {
                                 try {
                                     if (server.register(email, password, age)) {
-                                        MainActivity.this.showToast("Login has succeeded!");
+                                        MainActivity.this.showToast("Login was successful!");
+                                        setContentView(R.layout.home);
                                     } else {
                                         MainActivity.this.showToast("Login has failed!");
                                     }
